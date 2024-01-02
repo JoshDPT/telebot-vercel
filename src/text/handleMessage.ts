56729683f7fa079df174600d63ce4af5e36da7d0
JoshDPT <jjewcu@gmail.com>
2024-01-01 22:34:16 -0500

    update handleMessage for db insert

--- src/text/handleMessage.ts
+++ src/text/handleMessage.ts
@@ -1,9 +1,39 @@
 import { Context } from 'telegraf';
 import createDebug from 'debug';
-
-import { formatDate } from '../utils';
+import { connect } from '@planetscale/database';
 
 const debug = createDebug('bot:greeting_text');
+
+interface Response {
+  user_id: number;
+  date?: Date;
+  question: string;
+  response: string;
+}
+
+const saveResponseToDatabase = async (response: Response) => {
+  const config = {
+    host: process.env.DATABASE_HOST,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+  };
+
+  const conn = connect(config);
+
+  try {
+    // Insert response data into the database
+    await conn.execute(
+      `INSERT INTO responses (user_id, date, question, response)
+      VALUES (?, ?, ?, ?)`,
+      [response.user_id, response.date, response.question, response.response],
+    );
+
+    debug('Response data successfully saved to the database');
+  } catch (error) {
+    debug('Error saving response data to the database:', error);
+    throw new Error('Error saving response data to the database');
+  }
+};
 
 const replyToMessage = (ctx: Context, messageId: number, string: string) =>
   ctx.reply(string, {
@@ -18,19 +48,26 @@ const handleMessage = () => async (ctx: Context) => {
   // @ts-ignore
   if (ctx.update.message.reply_to_message !== undefined) {
     // @ts-ignore
-    const question = ctx.update.message.reply_to_message.text;
+    const question: string = ctx.update.message.reply_to_message.text;
     // @ts-ignore
     const response = ctx.update.message.text;
     // @ts-ignore
-    // const { id, first_name, last_name } = ctx.update.message.from;
+    const unixDate = ctx.update.message.date;
+    const timestampInMilliseconds = unixDate * 1000;
+    const date = new Date(timestampInMilliseconds) || new Date();
     // @ts-ignore
-    const date = formatDate(ctx.update.message.date);
+    const user_id = ctx.update.message.from.id;
 
-    // const update = ctx.update;
+    // Save the response to the database
+    await saveResponseToDatabase({
+      user_id,
+      date,
+      question,
+      response,
+    });
 
-    const text = `${question}\n\n"${response}"\n\n${date}`;
-
-    ctx.reply(text);
+    // You can reply with a confirmation message if needed
+    ctx.reply('Response saved successfully!');
   } else {
     if (messageId) {
       await replyToMessage(
